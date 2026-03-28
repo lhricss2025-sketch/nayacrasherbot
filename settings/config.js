c3d0dff8bfe3e7b2ce0b931487207fd9a2f908a1
senzo_12 <lhricss2025@gmail.com>
2026-03-28 12:07:22 +0500

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,8 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['ʏᴏᴜʀ ɴᴀᴍᴇ']
+global.packname = "Sticker By ᴅᴇᴍᴏɴ ᴇʏᴇ🕊"
+global.author = "ʟᴜᴄɪғᴇʀ ᴅᴇᴍᴏɴ"
+global.mess = {
+ owner: '*Lᴜᴄɪғᴇʀ ᴜᴘᴅᴀᴛᴇ ᴏғғɪᴄɪᴀʟ ᴏᴡɴᴇʀ*',
+ premium: '*You are not premium ᴜsᴇʀ*'
+}
